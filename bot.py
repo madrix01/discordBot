c5d7d5718fad289435022cbc0756813134c1d1dd
Shlok Patel <51414879+madrix01@users.noreply.github.com>
2020-03-02 16:21:40 +0530

    command update

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
 import discord
 from discord.ext import commands
-from cmd import cmdList, cmd_init
+from cmd import info
 from tokenki import tokenKey
 from pymongo import MongoClient
+import asyncio
 
 
 id = 683190065488199690
-#client = commands.Bot(command_prefix="#")
-client = discord.Client()
+client = commands.Bot(command_prefix=".")
+client.remove_command("help")
+#client = discord.Client()
 bot_id = "<@683176051773407282>"
 
 def listToString(s):
     str1 = " "    
     return str1.join(s)
+
 
 @client.event
 async def on_ready():
@@ -22,6 +25,7 @@ async def on_ready():
     print('------')
 
 
+
 @client.event
 async def on_member_join(member):
     await member.create_dm()
@@ -29,40 +33,23 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server! Type ".help" to see all the commands'
     )
 
-
 @client.event
 async def on_member_remove(member):
     print(f'bye bye {member}')
 
-@client.event
-async def on_message(message):
-    id = client.get_guild(683190065488199690)
-    channels = ['command']
-    msg = str(message.content)
-    print(msg)
-    
-    if msg[0] == cmd_init:
-        lst = msg.split()
-        inCmdBody = listToString(lst[1:])
-        print(inCmdBody)
-        inCmd = lst[0][1:]
-        if inCmd in cmdList['msg'].keys():
-            if inCmd == "hello":
-                await message.channel.send(cmdList['msg'][inCmd](message))
-            else:
-                await message.channel.send(cmdList["msg"][inCmd])
-        elif inCmd in cmdList['db'].keys():
-            if inCmd == "create":
-                if inCmdBody != "":
-                    post = {
-                        'message' : inCmdBody
-                    }
-                    cmdList['db']["create"](post)
-                    await message.channel.send("Saved! ;)")
-                else:
-                    await message.channel.send("Can't save blank message!")
-        else:
-            await message.channel.send("Pls enter a valid command! Type '.help' for all the commnads")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send(f"Hii {ctx.message.author}")
+
+
+@client.command()
+async def help(ctx):
+    await ctx.send(f"Hii {ctx.message.author} {info}")
+
+
+
+
 
 
 client.run(tokenKey)
--- cmd.py
+++ cmd.py
@@ -9,28 +9,12 @@ cmd_init = "."
 
 info = f"""
 Hello I am Dokkaebi, I am a bot created by Madrix.
-Type '{cmd_init}' in front of command to activate.
 commands-
-.help : All the info
+.helpme : All the info
 .hello : Greets
 .create <message> : Save message to the datatbase
 """
 
-
-def message_cmd(x):
-    return f"Hi {x.author}"
-
-
-
-cmdList = {
-    "msg" : {
-        'help' : info,
-        'hello' : message_cmd
-    },
-    "db" : {
-        'create' : save_data
-    }
-}
 
 
 
