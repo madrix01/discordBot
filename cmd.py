aaf27d28d9ae670ec5ddf8d3fa3e1e8f9a00d89b
Shlok Patel <51414879+madrix01@users.noreply.github.com>
2020-03-02 21:27:56 +0530

    heroku 3

--- cmd.py
+++ cmd.py
@@ -2,8 +2,7 @@ info = f"""
 Hello I am Dokkaebi, I am a bot created by Madrix.
 Commands-
     .helpme : All the info
-    .hello : Greets
-    .create <message> : Save message to the datatbase   
+    .hello : Greets   
 """
 
 
